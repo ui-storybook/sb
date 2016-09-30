31ce63bda6807bc8bd8d3db999a58eba584a5e33
freeman14 <dubovetzky@me.com>
2016-09-30 20:18:31 +0300

    Renamed webpack task to build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -53,7 +53,7 @@ let paths = {
 };
 
 // use webpack.config.js to build modules
-gulp.task('webpack', ['clean'], (cb) => {
+gulp.task('build', ['clean'], (cb) => {
   const config = require('./webpack.dist.config');
   config.entry.app = paths.entry;
 
