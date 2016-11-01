3b40df8db7241e18a29bce6ce233853e64edf2c0
freeman14 <dubovetzky@me.com>
2016-11-01 22:54:21 +0200

    Added mock API for SB

--- src/stories-api.js
+++ src/stories-api.js
@@ -2,8 +2,17 @@ class StoriesAPI {
 
     constructor() {
         this.sections = {};
+        this.mocks = {};
     }
 
+
+    /**
+     * Creta new section
+     * and return API instance that allow create stories
+     * 
+     * @param name Section name
+     * @returns function SB API instance
+     */
     section(name) {
         if (!this.sections[name]) {
             this.sections[name] = {};
@@ -29,8 +38,79 @@ class StoriesAPI {
 
     }
 
+
+    /**
+     * Get all savad sections
+     * 
+     * @returns object Sections
+     */
     getSections() {
         return this.sections;
+    }
+
+    /**
+     * Get story from store
+     * 
+     * @param sbObject Should contaie section, story name and point ID.
+     * @returns object Story
+     */
+    getStory(sbObject) {
+        try {
+            // Find story from store
+            let story = this.sections[sbObject.section][sbObject.story].find(i => i.id === sbObject.id);
+            console.log(story);
+            // Check if need load mock for story
+            if (story && typeof story.model === 'string') {
+                story.model = this.getMock(story.model);
+            }
+            return story;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+
+    /**
+     * Update story with new data
+     * Used for save tempalate and model
+     * Use this method if you need to edit any story param
+     * 
+     * @param sbObject Should contaie section, story name and point ID.
+     * @param data Story point that need update
+     */
+    updateStory(sbObject, data) {
+        let storyPoint = this.getStory(sbObject);
+        storyPoint = data;
+    }
+
+    /**
+     * Store mock for stories.
+     * Mock can be stored as object.
+     * 
+     * @param key Mock name
+     * @param value Mock value.
+     * @param performCB Function witch will call with mocked data
+     */
+    mock(key, value) {
+        if (typeof value === 'object') {
+            this.mocks[key] = value;
+        } else {
+            throw new Error(`Cant't save mock for ${key}. Mock value should be an object. Double check it.`);
+        }
+    }
+
+    /**
+     * Get mock from store
+     * 
+     * @param key Mock name
+     * @returns object Mock
+     */
+    getMock(key) {
+        if (this.mocks[key]) {
+            return this.mocks[key];
+        } else {
+            throw new Error(`Can't find mock for ${key}`);
+        }
     }
 
     registerContactCB(cb) {
@@ -43,19 +123,6 @@ class StoriesAPI {
 
     contact() {
         this.contactCB();
-    }
-
-    getStory(sbObject) {
-        try {
-            return this.sections[sbObject.section][sbObject.story].find(i => i.id === sbObject.id)
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
-    updateStory(sbObject, data) {
-        let storyPoint = this.getStory(sbObject);
-        storyPoint = data;
     }
 
     reload() {
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,134 +1,14 @@
+import './mock';
 
 let survey = sb.section('The week');
 survey.story('Names of the days of the week')
-  .add('Sunday', '<p>{{ vm.day }}</p>', {
-    day: 'Sunday'
-  })
-  .add('Monday', '<p>{{ vm.day }}</p>', {
-    day: 'Monday'
-  })
-  .add('Tuesday', '<p>{{ vm.day }}</p>', {
-    day: 'Tuesday'
-  })
-  .add('Wednesday', '<p>{{ vm.day }}</p>', {
-    day: 'Wednesday'
-  })
-  .add('Thursday', '<p>{{ vm.day }}</p>', {
-    day: 'Thursday'
-  })
-  .add('Friday', '<p>{{ vm.day }}</p>', {
-    day: 'Friday'
-  })
-  .add('Saturday', '<p>{{ vm.day }}</p>', {
-    day: 'Saturday'
-  })
-  .add('Sunday', '<p>{{ vm.day }}</p>', {
-    day: 'Sunday'
-  })
-  .add('Monday', '<p>{{ vm.day }}</p>', {
-    day: 'Monday'
-  })
-  .add('Tuesday', '<p>{{ vm.day }}</p>', {
-    day: 'Tuesday'
-  })
-  .add('Wednesday', '<p>{{ vm.day }}</p>', {
-    day: 'Wednesday'
-  })
-  .add('Thursday', '<p>{{ vm.day }}</p>', {
-    day: 'Thursday'
-  })
-  .add('Friday', '<p>{{ vm.day }}</p>', {
-    day: 'Friday'
-  })
-  .add('Saturday', '<p>{{ vm.day }}</p>', {
-    day: 'Saturday'
-  })
-  .add('Sunday', '<p>{{ vm.day }}</p>', {
-    day: 'Sunday'
-  })
-  .add('Monday', '<p>{{ vm.day }}</p>', {
-    day: 'Monday'
-  })
-  .add('Tuesday', '<p>{{ vm.day }}</p>', {
-    day: 'Tuesday'
-  })
-  .add('Wednesday', '<p>{{ vm.day }}</p>', {
-    day: 'Wednesday'
-  })
-  .add('Thursday', '<p>{{ vm.day }}</p>', {
-    day: 'Thursday'
-  })
-  .add('Friday', '<p>{{ vm.day }}</p>', {
-    day: 'Friday'
-  })
-  .add('Saturday', '<p>{{ vm.day }}</p>', {
-    day: 'Saturday'
-  });
-
-  survey.story('Some Other')
-  .add('Sunday', '<p>{{ vm.day }}</p>', {
-    day: 'Sunday'
-  })
-  .add('Monday', '<p>{{ vm.day }}</p>', {
-    day: 'Monday'
-  })
-  .add('Tuesday', '<p>{{ vm.day }}</p>', {
-    day: 'Tuesday'
-  })
-  .add('Wednesday', '<p>{{ vm.day }}</p>', {
-    day: 'Wednesday'
-  })
-  .add('Thursday', '<p>{{ vm.day }}</p>', {
-    day: 'Thursday'
-  })
-  .add('Friday', '<p>{{ vm.day }}</p>', {
-    day: 'Friday'
-  })
-  .add('Saturday', '<p>{{ vm.day }}</p>', {
-    day: 'Saturday'
-  })
-  .add('Sunday', '<p>{{ vm.day }}</p>', {
-    day: 'Sunday'
-  })
-  .add('Monday', '<p>{{ vm.day }}</p>', {
-    day: 'Monday'
-  })
-  .add('Tuesday', '<p>{{ vm.day }}</p>', {
-    day: 'Tuesday'
-  })
-  .add('Wednesday', '<p>{{ vm.day }}</p>', {
-    day: 'Wednesday'
-  })
-  .add('Thursday', '<p>{{ vm.day }}</p>', {
-    day: 'Thursday'
-  })
-  .add('Friday', '<p>{{ vm.day }}</p>', {
-    day: 'Friday'
-  })
-  .add('Saturday', '<p>{{ vm.day }}</p>', {
-    day: 'Saturday'
-  })
-  .add('Sunday', '<p>{{ vm.day }}</p>', {
-    day: 'Sunday'
-  })
-  .add('Monday', '<p>{{ vm.day }}</p>', {
-    day: 'Monday'
-  })
-  .add('Tuesday', '<p>{{ vm.day }}</p>', {
-    day: 'Tuesday'
-  })
-  .add('Wednesday', '<p>{{ vm.day }}</p>', {
-    day: 'Wednesday'
-  })
-  .add('Thursday', '<p>{{ vm.day }}</p>', {
-    day: 'Thursday'
-  })
-  .add('Friday', '<p>{{ vm.day }}</p>', {
-    day: 'Friday'
-  })
-  .add('Saturday', '<p>{{ vm.day }}</p>', {
-    day: 'Saturday'
-  });
+  .add('Sunday', '<p>{{ vm.sunday }}</p>', 'week')
+  .add('Monday', '<p>{{ vm.monday }}</p>', 'week')
+  .add('Tuesday', '<p>{{ vm.tuesday }}</p>', 'week')
+  .add('Wednesday', '<p>{{ vm.wednesday }}</p>', 'week')
+  .add('Thursday', '<p>{{ vm.thursday }}</p>', 'week')
+  .add('Friday', '<p>{{ vm.friday }}</p>', 'week')
+  .add('Saturday', '<p>{{ vm.saturday }}</p>', 'week');
 
 let test = sb.section('Demo 3');
 test.story('Test')
